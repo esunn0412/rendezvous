b455c2c1882ef1855045a6fdaa55d4066d59e3e0
TAEEUN KIM <taeeunk1208@gmail.com>
2025-01-12 04:42:34 +0900

    display list of HelloWorld models

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from accountapp.models import HelloWorld
 
 
 # Create your views here.
@@ -7,6 +10,15 @@ from django.shortcuts import render
 def hello_world(request):
 
     if request.method == 'POST':
-        return render(request, 'accountapp/helloworld.html', context={'text': 'POST METHOD!!!'})
+
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else :
-        return render(request, 'accountapp/helloworld.html', context={'text': 'GET METHOD!!!'})
+        hello_world_list = HelloWorld.objects.all()
+
+        return render(request, 'accountapp/helloworld.html', context={'hello_world_list': hello_world_list})
