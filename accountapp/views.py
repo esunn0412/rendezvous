7a1538e773f4d7d2558fa439f20401ea7f2ed99e
TAEEUN KIM <taeeunk1208@gmail.com>
2025-01-14 00:47:03 -0500

    fix error

--- accountapp/views.py
+++ accountapp/views.py
@@ -43,11 +43,13 @@ class AccountDetailView(DetailView):
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountUpdateForm
+    context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
 class AccountDeleteView(DeleteView):
     model = User
     success_url = reverse_lazy('accountapp:login')
+    context_object_name = 'target_user'
     template_name = 'accountapp/delete.html'
 
