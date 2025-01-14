ba577969c23f506c396712de99390bce08f4d709
TAEEUN KIM <taeeunk1208@gmail.com>
2025-01-14 02:48:06 -0500

    profile creation form validify

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
 
 # Create your views here.
+class ProfileCreateView(CreateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    context_object_name = 'target_profile'
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/create.html'
+
+    def form_valid(self, form):
+        tmp_profile = form.save(commit=False)
+        tmp_profile.user = self.request.user
+        tmp_profile.save()
+
+        return super().form_valid(form)
