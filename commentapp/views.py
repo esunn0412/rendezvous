e813d28238eac79de44c3a587569bb1def777f51
TAEEUN KIM <taeeunk1208@gmail.com>
2025-01-14 15:37:26 -0500

    comment display

--- commentapp/views.py
+++ commentapp/views.py
@@ -18,7 +18,7 @@ class CommentCreateView(CreateView):
 
         temp_comment = form.save(commit=False)
         temp_comment.article = Article.objects.get(pk=self.request.POST['article_pk'])
-        temp_comment.author = self.request.user
+        temp_comment.writer = self.request.user
         temp_comment.save()
 
         return super().form_valid(form)
