425e7afcb6d74370c14bd6be53ac5e1e532dbc7a
TAEEUN KIM <taeeunk1208@gmail.com>
2025-02-22 13:49:17 -0500

    fix home page errors

--- accountapp/views.py
+++ accountapp/views.py
@@ -11,32 +11,14 @@ from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
-from accountapp.models import HelloWorld
 from articleapp.models import Article
 
 has_ownership = [account_ownership_required, login_required]
 # Create your views here.
-@login_required
-def hello_world(request):
-    if request.method == 'POST':
-
-        temp = request.POST.get('hello_world_input')
-
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
-
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-    else :
-        hello_world_list = HelloWorld.objects.all()
-
-        return render(request, 'accountapp/helloworld.html', context={'hello_world_list': hello_world_list})
-
-#
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('home')
     template_name = 'accountapp/create.html'
 
 class AccountDetailView(DetailView, MultipleObjectMixin):
@@ -56,7 +38,7 @@ class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountUpdateForm
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('home')
     template_name = 'accountapp/update.html'
 
 
--- myPinterest/urls.py
+++ myPinterest/urls.py
@@ -23,7 +23,6 @@ from articleapp.views import ArticleListView
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='home'),
-
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
