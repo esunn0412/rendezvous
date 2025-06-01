a3245c493695b42b4a8ef9378ed4cb77195dbf65
TAEEUN KIM <taeeunk1208@gmail.com>
2025-06-02 01:55:23 +0900

    edit profile model

--- profileapp/models.py
+++ profileapp/models.py
@@ -7,7 +7,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     # if the user associated with profile is deleted, so will the profile
     # related name allows reference like request.user.profile
-    image = models.ImageField(upload_to='profile/', null=True)
+    image = models.ImageField(upload_to='profile/', null=True, blank=True)
     # beneath media directory like media/profile
-    nickname = models.CharField(max_length=20, unique=True, null=True)
-    message = models.CharField(max_length=100, null=True)
+    nickname = models.CharField(max_length=20, unique=True, null=True, blank=True)
+    message = models.CharField(max_length=100, null=True, blank=True)
